e97d4b03ee6671a076371322c978eecb0a3dd75b
以宁 <316167645@qq.com>
2022-07-13 12:04:25 +0800

    7.13上午

---
+++ src/07_翻转世界.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- 1. 变量准备-静态页面铺设 -->
+    <h1>{{ msg }}</h1>
+    <!-- 2. 绑定点击事件 -->
+    <button @click="btn">逆转世界</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello, World'
+    }
+  },
+  methods: {
+    btn () {
+      this.msg = this.msg.split(' ').reverse().join(' ')
+    }
+  }
+}
+</script>
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <div @click="fatherFn">
-    <!-- vue对事件进行了修饰符设置, 在事件后面.修饰符名即可使用更多的功能
-        .stop - 阻止事件冒泡
-     .prevent - 阻止默认行为
-         .once - 程序运行期间, 只触发一次事件处理函数-->
-    <button @click.stop="btn">.stop阻止事件冒泡</button>
-    <a href="http://www.baidu.com" @click.prevent="btn1"
-      >.prevent阻止默认行为</a
-    >
-    <button @click.prevent="btn3">
-      .once程序运行期间, 只触发一次事件处理函数
-    </button>
+  <div>
+    <!-- v-model:是实现vuejs 变量和表单标签value属性, 双向绑定的指令-->
+    <div>
+      <span>用户名:</span>
+      <input type="text" />
+    </div>
+    <div>
+      <span>密码: </span>
+      <input type="password" />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  methods: {
-    btn () {
-      console.log('btn')
-    },
-    btn1 () {
-      console.log('btn2')
-    },
-    btn3 () {
-      console.log('btn3')
+  data () {
+    return {
     }
   }
 }
