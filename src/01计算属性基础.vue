26cff17716143cf136a446983f1ff4dbb6ab67d0
以宁 <316167645@qq.com>
2022-07-16 14:37:52 +0800

    03上午

---
+++ src/01计算属性基础.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    {{ sum }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Demo11VueComputed',
+  // 计算属性:
+  // 场景: 一个变量的值, 需要用另外变量计算而得来
+  /*
+    语法:
+    computed: {
+      计算属性名 () {
+        return 值
+      }
+    }
+  */
+  // 注意: 计算属性和data属性都是变量-不能重名
+  // 注意2: 函数内变量变化, 会自动重新计算结果返回
+  data () {
+    return {
+      total: 5, // 商品数量
+      price: 98, // 单价
+      coupon: 400 // 优惠券
+    }
+  },
+  computed: {
+    sum () {
+      return this.total * this.price >= 500 ? this.total * this.price - this.coupon : this.total * this.price
+    }
+  }
+}
+</script>
---
+++ src/05侦听器.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <!-- watch侦听器: 作用可以侦听data/computed属性值改变, 当侦听的属性发生变化的时候，会触发对应的处理函数
+    语法：
+    watch: {
+      变量名 (newVal, oldVal){
+        // 变量名对应值改变这里自动触发
+      }
+    }-->
+    <input type="text" v-model="name" />
+    <input type="text" v-model="msg.age" />
+    <p>{{ msg.age }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueTest',
+  data () {
+    return {
+      name: '',
+      msg: {
+        age: 18
+      }
+    }
+  },
+  watch: {
+
+    'msg.age': {
+      deep: true,
+      handler (val, old) {
+        console.log(val)
+        console.log(old)
+      }
+    }
+  }
+}
+</script>
